0d886a25b00d2ab7467ebddfe7c25b9b797ecd0f
Faiz <66542302+FaizIbadurrahman@users.noreply.github.com>
2024-11-13 14:09:33 +0700

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,14 +1,15 @@
 import mysql.connector
 
 def connect_db():
-    """Connect to the MySQL database."""
+    """Connect to the remote MySQL database."""
     try:
         conn = mysql.connector.connect(
-            host="localhost",  # MySQL server host
-            user="root",  # MySQL username
-            password="",  # MySQL password
-            database="library_db"  # The database you want to connect to
+            host="srv1094.hstgr.io",
+            user="u268211096_siswa",
+            password="AlcentSiswa@4848",
+            database="elice"  # Replace with the correct database name if different
         )
+        print("Connected to MySQL Database")
         return conn
     except mysql.connector.Error as err:
         print(f"Error: {err}")
